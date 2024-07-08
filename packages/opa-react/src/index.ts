d153d5a22b8b49aea8bc833b68b047dd2b7a5c6c
Stephan Renatus <stephan@styra.com>
2024-07-08 10:58:55 +0200

    opa-react: export AuthzContext for SDK-usage
    Signed-off-by: Stephan Renatus <stephan@styra.com>

--- packages/opa-react/src/index.ts
+++ packages/opa-react/src/index.ts
@@ -1,6 +1,6 @@
 export { default as Authz } from "./authz.js";
 export type * from "./authz.js";
-export { default as AuthzProvider } from "./authz-provider.js";
+export { default as AuthzProvider, AuthzContext } from "./authz-provider.js";
 export type * from "./authz-provider.js";
 export { default as useAuthz } from "./use-authz.js";
 export type * from "./use-authz.js";
