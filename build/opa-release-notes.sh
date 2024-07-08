632dee808400e8a803e7eba777e08ff904f8ea83
Stephan Renatus <stephan@styra.com>
2024-07-08 09:41:50 +0200

    workflow/publish: amend release notes via script
    Using envsubst here should be a bit more robust than the echo call.
    Also, having it in a script makes it easier to maintain.
    
    Signed-off-by: Stephan Renatus <stephan@styra.com>

---
+++ build/opa-release-notes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This takes the last block (by record separator "\n## ") and removes the first line (timestamp)
+awk 'BEGIN { RS="\n## "} END { print $0 }' packages/opa/RELEASES.md | \
+  tail +2
+
+envsubst "$VERSION" <<EOF
+
+- [NPM ${VERSION}](https://www.npmjs.com/package/@styra/opa/v/${VERSION})"
+EOF
