12067d8c19822a622067e4fc01a9cb15949fcc5f
Stephan Renatus <stephan@styra.com>
2024-08-12 13:07:04 +0200

    build/opa-release-notes.sh: remove stray "

--- build/opa-release-notes.sh
+++ build/opa-release-notes.sh
@@ -5,5 +5,5 @@ awk 'BEGIN { RS="\n## "} END { print $0 }' packages/opa/RELEASES.md | \
   tail +2
 
 envsubst '$VERSION' <<EOF
-- [NPM ${VERSION}](https://www.npmjs.com/package/@styra/opa/v/${VERSION})"
+- [NPM ${VERSION}](https://www.npmjs.com/package/@styra/opa/v/${VERSION})
 EOF
